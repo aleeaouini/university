6c0006e034f54a61361f69548a83192bfe0e784e
Alee Aouini <aleeaouini@gmail.com>
2025-11-10 23:32:29 +0100

    update app.jsx

--- frontend/uni/src/App.jsx
+++ frontend/uni/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import './index.css';
+import { Edit } from 'lucide-react';
+import CsvPage from './pages/Csv';
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/edit' element={<Edit/>} />
+        <Route path='/csv' element={<CsvPage/>} />
       </Routes>
     </BrowserRouter>
   );
