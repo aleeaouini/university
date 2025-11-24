74963302052c0514780d22ef95cbf1628796697e
Alee Aouini <aleeaouini@gmail.com>
2025-11-24 19:17:28 +0100

    add route of student dashboard

--- frontend/uni/src/App.jsx
+++ frontend/uni/src/App.jsx
@@ -4,22 +4,34 @@ import Login from './pages/Login';
 import './index.css';
 import CsvPage from './pages/Csv';
 import Home from './pages/Home';
-
 import EditProfile from './pages/Editeprofile';
-
-
+import MainLayout from './layout/MainLayout';
+import Contact from './pages/Contact';
+import MessEns from './pages/MessEns';
+import ChefMessages from './pages/ListEns';
+import MessChef from './pages/MessChef';
+import StudentDashboard from './pages/StudentDashboard';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/login' element={<Login />}/>
+        
+        <Route element={<MainLayout />}>
         <Route path='/csv' element={<CsvPage/>} />
-        <Route path='/editprofile' element={<EditProfile />} />
+        <Route path='/editprofile' element={<EditProfile />}/>
         <Route path='/home' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/mess-ens' element={<MessEns />} />
+        //list ens
+        <Route path='/mess-chef' element={<ChefMessages />} />
+        <Route path='/mess-chef/:id_enseignant' element={<MessChef />} />
+        <Route path='/student' element={<StudentDashboard/>} />
+
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
